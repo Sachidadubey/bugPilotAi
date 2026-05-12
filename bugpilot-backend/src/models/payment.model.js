a13568a99457c8163b9d28696fa4903101efed53
Sachida Dhar Dubey <sachidanand2580@gmail.com>
2026-05-12 22:13:29 +0530

    Change userId type from ObjectId to String

--- bugpilot-backend/src/models/payment.model.js
+++ bugpilot-backend/src/models/payment.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const paymentSchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       ref: "User",
       required: false,
       index: true,
